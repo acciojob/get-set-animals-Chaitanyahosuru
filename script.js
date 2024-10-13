1d35ffc63c00196608b40344795819efd5956314
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-10-13 17:27:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species) {
+        this._species = species
+}
+	 get species() {
+        return this._species;
+	 }
+	makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species) {
+        super(species);
+		purr() {
+        console.log('purr');
+    }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	onstructor(species) {
+        super(species); // Call the parent class constructor
+    }
+	bark() {
+        console.log('woof');
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
